b307e7d8f7555efd2b5c579a3df27debaf930635
devopsor <saekiemail@gmail.com>
2022-09-28 00:32:01 +0900

    update hellowolrd

--- 01_helloworld/index.php
+++ 01_helloworld/index.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<?php
-  print "hello world<br>";
-?>
-</body>
+    <head>
+        <title>HelloWorld</title>
+    </head>
+    <body>
+        <?php
+            // echo and print are more or less the same. They are both used to output data to the screen.
+            echo "<h2>PHP is Fun!</h2>";
+            echo "Hello world!<br>";
+            echo("Hello world!<br>");
+            echo "I'm about to learn PHP!<br>";
+            // echo accepts multiple arguments. So you may write echo $a, $b instead of echo $a . $b
+            echo "This ", "string ", "was ", "made ", "with multiple parameters.", "<br>";
+            echo("This " . "string " . "was " . "made " ."with multiple parameters."."<br>");
+            
+            print "<h2>PHP is Fun!</h2>";
+            print "Hello world!<br>";
+            print "I'm about to learn PHP!";
+
+           
+            $text = "Learn PHP";
+            $x = 5;
+            $y = 4;
+
+            print "<h2>" . $text . "</h2>";
+            print("<h2>" . $text . "</h2>");
+            // echo has no return value 
+            // $test = echo $x+$y;          // Parse Error
+             // print has a return value of 1 so it can be used in expressions
+            print $x + $y;  // 9
+            print "<br>";
+            $result = print $x + $y;  // 9
+            print "<br>";
+            print $result;  // 1
+            print "<br>";
+            print($result); // 1
+
+        ?>
+    </body>
 </html>
---
+++ 02_predefined/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Predefined</title>
+    </head>
+    <body>
+        <?php
+            $a = 1;
+            // References all variables available in global scope
+            print_r($GLOBALS);
+            print_r($_SERVER['REQUEST_URI']);
+            print_r($_GET);
+        ?>
+    </body>
+</html>
